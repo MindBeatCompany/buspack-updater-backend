ddc7612d064f6915b93b0cff3d84f166cfe785f3
JuanCruzReyAlvarez <82044262+JuanCruzReyAlvarez@users.noreply.github.com>
2024-09-16 00:06:11 -0300

    Update SaitDestinations.py

--- buspackProcessBackendAPI/src/sait/saitDestinations/SaitDestinations.py
+++ buspackProcessBackendAPI/src/sait/saitDestinations/SaitDestinations.py
@@ -2,7 +2,7 @@
 class SaitDestinations:
     def __init__(self, tokenResponse, isActive):
         self.activos = isActive
-        self.url = 'https://testrest.empresar-sys.com.ar:1434/sait/destinosHabilitados'
+        self.url = 'https://rest.empresar-sys.com.ar:1433/sait/destinosHabilitados'
         self.token = tokenResponse.json().get('token')
 
     def parametersFormData(self):
